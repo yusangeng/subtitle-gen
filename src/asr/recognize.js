af1b8b3a59388a21ceaa6b00c0a8cbc1be349369
yusangeng <yusangeng@outlook.com>
2019-08-03 18:57:57 +0800

    捕获语音识别异常，后在srt文件中添加占位信息

--- src/asr/recognize.js
+++ src/asr/recognize.js
@@ -15,13 +15,28 @@ module.exports = async function recognize (timeline) {
     const { filename, startTime, duration } = tl.shift()
     const sliceData = fs.readFileSync(filename)
     const sliceBuffer = Buffer.from(sliceData)
+    let response
 
-    const response = await client.recognize(sliceBuffer, 'pcm', 16000)
+    try {
+      response = await client.recognize(sliceBuffer, 'pcm', 16000)
+    } catch (err) {
+      response = {
+        err_no: -1,
+        err_msg: `网络或服务器错误(${err.message})`
+      }
+    }
+
     const { err_no: errNo, err_msg: errMsg, result } = response
 
     if (errNo) {
       error(`识别错误: ${errMsg}`)
       error(`输入文件: ${filename}`)
+      slices.push({
+        text: '',
+        start: startTime,
+        end: startTime + duration,
+        duration
+      })
     } else {
       const text = result.join('')
       info(`识别结果: ${text}`)
